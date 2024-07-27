34c1cc1e40a4bd3d1270415170ae4d62210bea68
Mark Story <mark@mark-story.com>
2024-07-27 23:05:39 +0200

    Trim unused code out.

--- src/FeatureFlagsPlugin.php
+++ src/FeatureFlagsPlugin.php
@@ -16,55 +16,6 @@ use Cake\Routing\RouteBuilder;
 class FeatureFlagsPlugin extends BasePlugin
 {
     /**
-     * Load all the plugin configuration and bootstrap logic.
-     *
-     * The host application is provided as an argument. This allows you to load
-     * additional plugin dependencies, or attach events.
-     *
-     * @param \Cake\Core\PluginApplicationInterface $app The host application
-     * @return void
-     */
-    public function bootstrap(PluginApplicationInterface $app): void
-    {
-    }
-
-    /**
-     * Add routes for the plugin.
-     *
-     * If your plugin has many routes and you would like to isolate them into a separate file,
-     * you can create `$plugin/config/routes.php` and delete this method.
-     *
-     * @param \Cake\Routing\RouteBuilder $routes The route builder to update.
-     * @return void
-     */
-    public function routes(RouteBuilder $routes): void
-    {
-        $routes->plugin(
-            'FeatureFlags',
-            ['path' => '/feature-flags'],
-            function (RouteBuilder $builder) {
-                // Add custom routes here
-
-                $builder->fallbacks();
-            }
-        );
-        parent::routes($routes);
-    }
-
-    /**
-     * Add middleware for the plugin.
-     *
-     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to update.
-     * @return \Cake\Http\MiddlewareQueue
-     */
-    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
-    {
-        // Add your middlewares here
-
-        return $middlewareQueue;
-    }
-
-    /**
      * Add commands for the plugin.
      *
      * @param \Cake\Console\CommandCollection $commands The command collection to update.
@@ -73,21 +24,10 @@ class FeatureFlagsPlugin extends BasePlugin
     public function console(CommandCollection $commands): CommandCollection
     {
         // Add your commands here
-
         $commands = parent::console($commands);
 
-        return $commands;
-    }
+        // TODO add feature flag validation tool?
 
-    /**
-     * Register application container services.
-     *
-     * @param \Cake\Core\ContainerInterface $container The Container to update.
-     * @return void
-     * @link https://book.cakephp.org/4/en/development/dependency-injection.html#dependency-injection
-     */
-    public function services(ContainerInterface $container): void
-    {
-        // Add your services here
+        return $commands;
     }
 }
